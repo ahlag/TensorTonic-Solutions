6b97b2b21b9cee783adab4913a83ddde78cb950e
Chang Yong Lik <51813538+ahlag@users.noreply.github.com>
2026-05-11 19:14:06 +0900

    Update solution for pytorch-tensor-creation

--- study-plans/pytorch-basics/pytorch-tensor-creation/pytorch-tensor-creation.py
+++ study-plans/pytorch-basics/pytorch-tensor-creation/pytorch-tensor-creation.py
@@ -5,8 +5,10 @@ def create_tensor(method, shape, value=0.0):
     Returns: list
     """
     if method == "zeros":
-        return torch.zeros(shape).tolist()
+        return torch.zeros(shape)
     elif method == "ones":
-        return torch.ones(shape).tolist()
+        return torch.ones(shape)
     elif method == "full":
-        return torch.full(shape, value).tolist()
+        return torch.full(shape, value)
+    else:
+        return None
