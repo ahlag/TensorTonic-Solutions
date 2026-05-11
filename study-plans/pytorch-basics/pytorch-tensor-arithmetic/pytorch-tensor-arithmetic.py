c823b18a25a5f4770837fe6e63c05169bd5f8611
Chang Yong Lik <51813538+ahlag@users.noreply.github.com>
2026-05-11 19:19:04 +0900

    Update solution for pytorch-tensor-arithmetic

--- study-plans/pytorch-basics/pytorch-tensor-arithmetic/pytorch-tensor-arithmetic.py
+++ study-plans/pytorch-basics/pytorch-tensor-arithmetic/pytorch-tensor-arithmetic.py
@@ -7,15 +7,19 @@ def tensor_op(x, y, op):
     x = torch.tensor(x)
     y = torch.tensor(y)
 
+    op = op.lower()
+
     if op == "add":
         result = torch.add(x, y)
-    elif op == "multiply":
-        result = torch.multiply(x, y)
+    elif op == "mul" or op == "multiply":
+        result = torch.mul(x, y)
     elif op == "matmul":
         result = torch.matmul(x, y)
     elif op == "power":
         result = torch.pow(x, y)
     elif op == "max":
         result = torch.max(x, y)
+    else:
+        return None
 
     return result.tolist()
